3d5e25365e3a06f488beee318fe26adbfa7a9ddb
Dean Schwartz <deanschwartz2@gmail.com>
2021-07-08 13:07:40 -0700

    took out comments

--- src/polybius.js
+++ src/polybius.js
@@ -52,22 +52,6 @@ const polybiusModule = (function () {
       } else {
         return false;
       }
-
-
-      // const key = {"a": "11", "b": "21", "c": "31", "d": "41", "e": "51", "f": "12", "g": "22", "h": "32", "(i/j)": "42", "k": "52", "l": "13", "m": "23", "n": "33", "o": "43", "p": "53", "q": "14", "r": "24", "s": "34", "t": "44", "u": "54", "v": "15", "w": "25", "x": "35", "y": "45", "z": "55"};
-      // const letters = Object.keys(key);
-      // const numbers = Object.values(key);
-      // const numberGroups = input.split(" ");
-      // const result = numberGroups.map((numberGroup) => {
-      //   //find index of the number group to decode
-      //   const codedLetters = numberGroup.match(/.{1,2}/g);
-      //   const decodedWord = codedLetters.map((codedLetter) => {
-      //     const index = numbers.indexOf(codedLetter);
-      //     return letters[index];
-      //   });
-      //   return decodedWord.join("");
-      // });
-      // return result.join(" ");
     }
   }
 
